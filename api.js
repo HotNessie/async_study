59ebafc6c155dd29c6d4b4ca5cda03272360df9e
HotNessie <fkdhswpsk7@gmail.com>
2024-08-29 17:41:34 +0900

    trry

--- api.js
+++ api.js
@@ -1,17 +1,22 @@
 const form = document.querySelector("#searchForm");
 
 form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    //입력값을 받고 같은 페이지에서 사진을 보여줄거라 이벤트 종료
-    const searchTerm = form.elements.query.value
-    //console.dir(form)제출해서 form하위 elements 확인 input이름을
-    const config = { params: { q: searchTerm } }
-    //query로 저장했으니까 그거 찾아서 value값 받아오기
-    const res = await axios.get(`url`, config)
-    //api를 받아올 사이트에 input입력값 보내기 (쿼리가 어떻게 짜여있나 봐야됨)
-    makeImages(res.data)
-    form.elements.query.value = ""
+    try {
+        e.preventDefault();
+        //입력값을 받고 같은 페이지에서 사진을 보여줄거라 이벤트 종료
+        const searchTerm = form.elements.query.value
+        //console.dir(form)제출해서 form하위 elements 확인 input이름을
+        const config = { params: { q: searchTerm } }
+        //query로 저장했으니까 그거 찾아서 value값 받아오기
+        const res = await axios.get(`url`, config)
+        //api를 받아올 사이트에 input입력값 보내기 (쿼리가 어떻게 짜여있나 봐야됨)
+        makeImages(res.data)
+        form.elements.query.value = ""
+    } catch (error) {
+        console.log('error', error)
+    }
 })
+
 
 const makeImages = (shows) => {
 
